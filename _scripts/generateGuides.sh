518f25a5f13192a6ad3db602cfdd56d2c86ceb8b
Paul Jolly <paul@myitcv.io>
2020-05-04 15:59:25 +0100

    guides: prepare for the first guide
    Various changes to prepare for the next PR which will be the first full
    proof-of-concept guide.
    
    For now we use the -compat flag to preguide until the necessary changes
    to the SDK have been made.

---
+++ _scripts/generateGuides.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+eval "$($( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/env.sh bash)"
+
+cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+go generate -tags guides ./guides
--- js/post-layout.js
+++ js/post-layout.js
@@ -1,102 +1,123 @@
 ---
 ---
+function replaceInText(element, replacements) {
+	for (let node of element.childNodes) {
+		switch (node.nodeType) {
+			case Node.ELEMENT_NODE:
+				replaceInText(node, replacements);
+				break;
+			case Node.TEXT_NODE:
+				for (let r of replacements) {
+					node.textContent = node.textContent.replaceAll(r[0], r[1]);
+				}
+				break;
+			case Node.DOCUMENT_NODE:
+				replaceInText(node, replacements);
+		}
+	}
+}
 var siteUrl = "{{ site.url }}";
 var fontChanged = false;
 var pwd = new PWD();
 var guideRequest = new XMLHttpRequest();
-// TODO: move away from hard-code guides URL to use {{ page.guide }}
-guideRequest.open('GET', '{{site.pwdurl}}/guides/2019-03-19-using-go-modules');
-guideRequest.withCredentials = true;
-guideRequest.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+// TODO: move away from hard-code guides URL to use a variable
+// specified in the site config
+guideRequest.open('POST', '{{ site.controllerurl }}');
+guideRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 guideRequest.onload = function() {
-	console.log("We got a response", guideRequest)
 	// TODO: error handling
 	if (guideRequest.status != 200) {
-		return;
+		throw "Failed to make guides request";
 	}
-	var sessionData = JSON.parse(guideRequest.responseText);
-	pwd.init(
-		sessionData.session_id,
-		{ baseUrl: "{{site.pwdurl}}", ImageName: imageName, oauthProvider: 'google' },
-		function() {
-			var instances = [];
-			for (name in pwd.instances) {
-				instances.push(pwd.instances[name]);
+	var guideDetails = JSON.parse(guideRequest.responseText);
+
+	let replacements = [];
+	for (let env of guideDetails.Env) {
+		let parts = env.split("=", 2);
+		replacements.push([guideDetails.Delims[0]+"."+parts[0]+guideDetails.Delims[1], parts[1]]);
+	}
+	replaceInText(document.querySelector("article"), replacements);
+	for (let attr of ["data-upload-src", "data-command-src"]) {
+		document.querySelectorAll("["+attr+"]").forEach(function(node) {
+			let v = atob(node.getAttribute(attr));
+			for (let r of replacements) {
+				v = v.replaceAll(r[0], r[1]);
 			}
-			pwd.createTerminal({ selector: ".term1" }, instances[0].name);
-		}
-	);
+			node.setAttribute(attr, btoa(v));
+		});
+	}
+	pwd.newSession(guideDetails.Terminals, { baseUrl: "{{site.pwdurl}}", oauthProvider: 'google', Networks: guideDetails.Networks, Envs: guideDetails.Env });
 };
-guideRequest.send();
+guideRequest.send(JSON.stringify({Guide: pageGuide, Language: pageLanguage, Scenario: pageScenario}));
 $(".panel-left").resizable({
-  handleSelector: ".splitter",
-  resizeHeight: false,
-  onDragEnd: pwd.resize.bind(pwd)
+	handleSelector: ".splitter",
+	resizeHeight: false,
+	onDragEnd: pwd.resize.bind(pwd)
 });
 
 $(document).ready(function() {
-  $(".nav a").on("click", function() {
-    let sel = $(".tab-pane.active").get(0);
-    sel.style.display = "none";
-    sel.offsetHeight; // no need to store this anywhere, the reference is enough
-    sel.style.display = "";
-  });
-  //Resize when switching terminals
-  $(".nav-tabs .nav-item").on("click",()=>{
-    pwd.resize()
-  })
-  // expand/shrink terminals
-  $(".fa-expand").on("click", function() {
-    const leftPanel = $(".panel-left");
-    const menuPanel = $(".menu-panel");
-    leftPanel.toggle();
-    menuPanel.hide();
-    const terminalOpened = leftPanel.is(":hidden");
-    // add compress icon if leftPanel is open
-    $(this).toggleClass("fa-compress", terminalOpened);
-    setMenuButtonIcon();
-    pwd.resize();
-  });
+	$(".nav a").on("click", function() {
+		let sel = $(".tab-pane.active").get(0);
+		sel.style.display = "none";
+		sel.offsetHeight; // no need to store this anywhere, the reference is enough
+		sel.style.display = "";
+	});
+	//Resize when switching terminals
+	$(".nav-tabs .nav-item").on("click",()=>{
+		pwd.resize()
+	})
+	// expand/shrink terminals
+	$(".fa-expand").on("click", function() {
+		const leftPanel = $(".panel-left");
+		const menuPanel = $(".menu-panel");
+		leftPanel.toggle();
+		menuPanel.hide();
+		const terminalOpened = leftPanel.is(":hidden");
+		// add compress icon if leftPanel is open
+		$(this).toggleClass("fa-compress", terminalOpened);
+		setMenuButtonIcon();
+		pwd.resize();
+	});
 
-  // toggle menu
-  $(".course-index-button").on("click", function() {
-    const menuPanel = $(".menu-panel");
-    menuPanel.toggle();
-    setMenuButtonIcon();
-  });
+	// toggle menu
+	$(".course-index-button").on("click", function() {
+		const menuPanel = $(".menu-panel");
+		menuPanel.toggle();
+		setMenuButtonIcon();
+	});
 
-  const setMenuButtonIcon = () => {
-    const menuPanel = $(".menu-panel");
-    const menuButtom = $(".menu-button");
-    const menuIsOpen = menuPanel.is(":visible");
-    menuButtom.toggleClass("fa-times", menuIsOpen);
-    menuButtom.toggleClass("fa-bars", !menuIsOpen);
-  };
+	const setMenuButtonIcon = () => {
+		const menuPanel = $(".menu-panel");
+		const menuButtom = $(".menu-button");
+		const menuIsOpen = menuPanel.is(":visible");
+		menuButtom.toggleClass("fa-times", menuIsOpen);
+		menuButtom.toggleClass("fa-bars", !menuIsOpen);
+	};
 
-  /*$('.btn-text').on('click', function(){
-          if (fontChanged) {
-              $('.terminal').css('font-size', '15px').css('line-height', '17px');
-          } else {
-              $('.terminal').css('font-size', '20px').css('line-height', '22px');
-          }
-          $(window).trigger('resize');
-          fontChanged = !fontChanged;
-      });*/
+	/*$('.btn-text').on('click', function(){
+					if (fontChanged) {
+							$('.terminal').css('font-size', '15px').css('line-height', '17px');
+					} else {
+							$('.terminal').css('font-size', '20px').css('line-height', '22px');
+					}
+					$(window).trigger('resize');
+					fontChanged = !fontChanged;
+			});*/
 });
 
 // progress bar
 
 function scrollHandler() {
-  const scrollTop = $(this).scrollTop();
-  const scrollHeight = $(this).prop("scrollHeight");
-  const height = $(this).height();
-  const progress = ((scrollTop + height) / scrollHeight) * 100;
-  const entireProgress = Math.ceil(scrollTop ? progress : 0);
+	const scrollTop = $(this).scrollTop();
+	const scrollHeight = $(this).prop("scrollHeight");
+	const height = $(this).height();
+	const progress = ((scrollTop + height) / scrollHeight) * 100;
+	const entireProgress = Math.ceil(scrollTop ? progress : 0);
 
-  $(".progress-bar").width(entireProgress + "%");
-  $(".progress-bar").attr("data-progress", entireProgress || null);
+	$(".progress-bar").width(entireProgress + "%");
+	$(".progress-bar").attr("data-progress", entireProgress || null);
 }
 
 $(() => {
-  $(".post.content").on("scroll", scrollHandler);
+	$(".post.content").on("scroll", scrollHandler);
 });
