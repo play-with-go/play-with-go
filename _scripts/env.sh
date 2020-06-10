e2db30158cf77dd750879069d9095b414d376f73
Paul Jolly <paul@myitcv.io>
2020-06-10 16:03:21 +0100

    deps: add a dependency on github.com/play-with-docker/play-with-docker
    Depending on a specific version of PWD means we are explicit about the
    version of backend we are depending on.
    
    For now we depend on a fork of PWD whilst we await a number of PRs to be
    merged.

--- _scripts/env.sh
+++ _scripts/env.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 source "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/env_common.bash
 
 modRoot="$(go list -m -f {{.Dir}})"
+pwdRoot="$(go list -m -f {{.Dir}} github.com/play-with-docker/play-with-docker)"
 
 GOBIN=$modRoot/.bin go install github.com/myitcv/docker-compose
 
 $export COMPOSE_PROJECT_NAME playwithgo
+$export COMPOSE_FILE "$pwdRoot/docker-compose.yml:$modRoot/docker-compose.yml"
 $export PATH "$modRoot/.bin:$PATH"
--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.15
 require (
 	cuelang.org/go v0.2.2
 	github.com/myitcv/docker-compose v0.0.0-20200623052903-c60483a3250f
+	github.com/play-with-docker/play-with-docker v0.0.2-0.20200622012609-cf50d344b2d8
 	github.com/play-with-go/preguide v0.0.0-20200828095858-798c12361b4c
 )
+
+replace github.com/play-with-docker/play-with-docker => github.com/myitcvforks/play-with-docker v0.0.2-0.20200827184138-85ca8fc6c99b
