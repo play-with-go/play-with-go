2c581b9a1466b821a6bedd3c72aa96c499f0935e
Paul Jolly <paul@myitcv.io>
2020-09-08 18:13:11 +0100

    general: add license (#71)
    Fixes #51

--- internal/deps/gen.go
+++ internal/deps/gen.go
@@ -1,3 +1,7 @@
+// Copyright 2020 The play-with-go.dev Authors. All rights reserved.  Use of
+// this source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
 package deps
 
 //go:generate go run cuelang.org/go/cmd/cue cmd vendorgithubschema .
