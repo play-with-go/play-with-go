1bfa7090899443b5cbba1628bbc936d3d4dbff3d
Paul Jolly <paul@myitcv.io>
2020-09-28 12:40:20 +0100

    all: use inherit_errexit in bash scripts for safety
    Also upgrade CI workflow to use Go 1.15.2

--- _scripts/env.sh
+++ _scripts/env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+shopt -s inherit_errexit
 
 source "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/env_common.bash
 
--- _scripts/generateEverythingElse.sh
+++ _scripts/generateEverythingElse.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+shopt -s inherit_errexit
 
 eval "$($( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/env.sh bash)"
 
