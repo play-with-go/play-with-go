88dce1f3cb7963edb3931839f5c26d5c20e75541
Paul Jolly <paul@myitcv.io>
2020-10-01 05:46:58 +0100

    Drop "bad" check

--- _scripts/setupmkcert.sh
+++ _scripts/setupmkcert.sh
@@ -3,5 +3,8 @@
 set -eu -o pipefail
 shopt -s inherit_errexit
 
+# Ensure we have the relevant module available
+go mod download github.com/play-with-go/gitea
+
 # Simply delegate to the gitea script
 bash $(go list -m -f {{.Dir}} github.com/play-with-go/gitea)/_scripts/setupmkcert.sh
