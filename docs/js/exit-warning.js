05cf8874a11f7b1304bc2a283020bb89e131fe1c
Paul Jolly <paul@myitcv.io>
2020-10-15 14:34:46 +0100

    all: remove CNAME and docs; we are publishing in another repo (#137)

--- docs/js/exit-warning.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('beforeunload', function (e) {
-  navigator.sendBeacon("https://api.play-with-go.dev/sessions/" + pwd.sessionId + "/close");
-  // Cancel the event
-  e.preventDefault();
-  // Chrome requires returnValue to be set
-  e.returnValue = '';
-});
-
