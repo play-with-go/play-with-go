08801f42fd6bb724ce8d2af08630b98f2af30504
Paul Jolly <paul@myitcv.io>
2020-10-15 14:26:35 +0100

    all: remove CNAME and docs; we are publishing in another repo

--- docs/js/dark-mode.js
+++
@@ -1,7 +0,0 @@
-$(()=>{
-  $(".dark-mode").click(()=>{
-    $(document.body).toggleClass("dark-mode");
-    const isDark = $(document.body).hasClass("dark-mode");
-    ga('send', 'event', 'Nav', 'dark', isDark ? "enabled": "disabled");
-  })
-})
