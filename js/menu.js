7c9c7b3e451733ea20cd2758570a3fdcd59f84c9
Marcos Lilljedahl <marcosnils@gmail.com>
2020-10-26 03:20:35 -0300

    Add link to github issues

--- js/menu.js
+++ js/menu.js
@@ -7,23 +7,4 @@ $(() => {
      ga('send', 'event', 'Menu', 'click-help');
     $("#helpModal").modal();
   });
-
-  $("#helpMessage").keyup(() => {
-    if ($("#helpMessage")[0].checkValidity()) {
-      $("#sendHelp").prop("disabled", false);
-    } else {
-      $("#sendHelp").prop("disabled", true);
-    }
-  });
-  $("#sendHelp").click(() => {
-     ga('send', 'event', 'Feedback', 'send-bug');
-    $("#helpModal .modal-body").html(`
-<h5 class="modal-title feedback-title">Message sent successfully.</h5>
-<div class="row">
-  <div class="col-sm">
-    <button type="button" data-dismiss="modal" class="btn btn-link float-right">CLOSE</button>
-  </div>
-</div>
-`);
-  });
 });
